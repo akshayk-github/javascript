9cd1dcbd07a8cd7b3cac762389882aaa1ce3a27c
akshayk-github <104121315+akshayk-github@users.noreply.github.com>
2023-09-24 10:44:42 +0000

    variables

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 1234;
+let accountName = "Akshay kumbhani"
+var accountPassword = "74108520963."
+accountCity = "Surat"
+let accountState;
+
+console.log(accountId);
+
+console.table([accountId, accountName, accountPassword, accountCity, accountState])
