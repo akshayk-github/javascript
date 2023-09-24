8a7b32aadf2058087bc3001e9ed15906064c2c95
akshayk-github <104121315+akshayk-github@users.noreply.github.com>
2023-09-24 10:44:58 +0000

    datatypes

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,10 @@
+// number => 1234
+// null => standalone value
+// boolean => true/false
+// bigint => 
+// String => ""
+// symbol => unique
+// undefined => 
+
+console.log(typeof undefined); // type = undefined
+console.log(typeof null); // type = object
