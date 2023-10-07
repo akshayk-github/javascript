32e614270f5b69e74cc218cbecd8d1d4a7297348
akshayk-github <104121315+akshayk-github@users.noreply.github.com>
2023-10-07 05:44:04 +0000

    comparison

---
+++ 01_basic/04_comparision.js
@@ -0,0 +1,28 @@
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 <= 1);
+// console.log(2 >= 1);
+// console.log(2 === 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+/* console.log(null > 0);
+console.log(null == 0);              // This is very confucing code
+console.log(null >= 0);
+
+let h = null
+
+let g = Number(h)
+
+console.log(g); */ 
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+console.log("2" === 2);
+console.log("2" == 2);
+console.log(2 === 2);
